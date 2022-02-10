18f2e8033e717cbd5a47e8429799dccd7f070537
marynghiem <marynghiem46@gmail.com>
2022-02-10 12:56:30 -0800

    added drop down button for navbar and then added css for calorie intake page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -17,9 +17,14 @@ export const NavBar = () => {
         <a href="/dinner" className="navItem">
           DINNER <i className="fa fa-caret-down"></i>
         </a>
-        <a href="/mealplan" className="navItem">
-          MEAL PLAN <i className="fa fa-caret-down"></i>
-        </a>
+        <div class="navItem dropdown">
+          <button class="dropbtn">
+            MEAL PLAN <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+            <a href="/calorieIntake">Meal Prep By Calories</a>
+          </div>
+        </div>
       </div>
     </div>
   );
