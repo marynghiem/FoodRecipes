718c36a68450ca71e090c7173f93b22c2c06f27a
marynghiem <marynghiem46@gmail.com>
2022-03-01 12:08:22 -0800

    changed breakfast css to breakfast styled components

--- client/src/components/Breakfast/Breakfast.js
+++ client/src/components/Breakfast/Breakfast.js
@@ -1,6 +1,12 @@
 import React from "react";
-import "../../css/Breakfast/Breakfast.css";
+import styled from "styled-components";
+
+const BreakfastContainer = styled.div`
+  background-color: rgb(255, 225, 230);
+  height: 130vh;
+  width: 100vw;
+`;
 
 export const Breakfast = () => {
-  return <div className="breakfastContainer"></div>;
+  return <BreakfastContainer></BreakfastContainer>;
 };
