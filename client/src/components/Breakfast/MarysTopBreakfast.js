6dd3d1013bd3bc46d89422ff6ab68ddda51ad285
marynghiem <marynghiem46@gmail.com>
2022-03-03 15:53:25 -0800

    added background for breakfast component

--- client/src/components/Breakfast/MarysTopBreakfast.js
+++ client/src/components/Breakfast/MarysTopBreakfast.js
@@ -1,5 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+
+const MarysTopBreakfastContainer = styled.div`
+  background-color: #dcd0ff;
+  height: 130vh;
+  width: 100vw;
+`;
 
 export const MarysTopBreakfast = () => {
-  return <div></div>;
+  return <MarysTopBreakfastContainer></MarysTopBreakfastContainer>;
 };
