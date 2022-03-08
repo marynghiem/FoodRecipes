25918333f1d8929c284a637c5f95bccb1c7c4ae6
marynghiem <marynghiem46@gmail.com>
2022-03-08 14:06:38 -0800

    added dinformation from the api to dessert component

--- client/src/components/Dessert/Dessert.js
+++ client/src/components/Dessert/Dessert.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const DessertContainer = styled.div`
   background-image: url("dessertBackground.jpeg");
-  height: 130vh;
+  height: 300vh;
 `;
 const Container = styled.div`
-  width: 100px;
-  height: 100px;
+  width: 1000px;
   background-color: white;
+  margin: auto;
+`;
+
+const DessertItemContainer = styled.div`
+  display: inline-block;
 `;
 
 export const Dessert = () => {
+  const url = "/food_type_results?mealtype=dessert";
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((exception) => console.error(exception));
+  }, []);
+
   return (
     <DessertContainer>
-      <Container />
+      <Container>
+        {data?.results?.length > 0 &&
+          data.results.map((result) => (
+            <DessertItemContainer>
+              <img src={result.image} alt={result.title}></img>
+              <div>{result.title}</div>
+            </DessertItemContainer>
+          ))}
+      </Container>
     </DessertContainer>
   );
 };
