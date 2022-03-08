be0bc7596155bf83ad29b76c68904006e0916bf2
marynghiem <marynghiem46@gmail.com>
2022-03-07 16:16:11 -0800

    added main course information from api

--- client/src/components/Dinner/MainCourse.js
+++ client/src/components/Dinner/MainCourse.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const MainCourseContainer = styled.div`
+  height: 250vh;
+  width: 100vw;
+  background-color: #dbf3fa;
+`;
+
+const MainCourseItemContainer = styled.div`
+  display: inline-block;
+`;
 
 export const MainCourse = () => {
-  return <div></div>;
+  const url = `/food_type_results?mealtype=main course`;
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((exception) => console.error(exception));
+  }, []);
+
+  return (
+    <MainCourseContainer>
+      {data?.results?.length > 0 &&
+        data.results.map((result) => (
+          <MainCourseItemContainer>
+            <img src={result.image} alt={result.title}></img>
+            <div>{result.title}</div>
+          </MainCourseItemContainer>
+        ))}
+    </MainCourseContainer>
+  );
 };
