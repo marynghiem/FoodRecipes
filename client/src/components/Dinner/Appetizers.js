74074e581a1c9a55abc2a4ebb8bf2f8c78fb7bd4
marynghiem <marynghiem46@gmail.com>
2022-03-07 16:39:19 -0800

    added information from api to appetizer and side dish route

--- client/src/components/Dinner/Appetizers.js
+++ client/src/components/Dinner/Appetizers.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const AppetizerContainer = styled.div`
+  height: 200vh;
+  width: 100vw;
+  background-color: #cfebfd;
+`;
+
+const AppetizerItemContainer = styled.div`
+  display: inline-block;
+`;
 
 export const Appetizers = () => {
-  return <div></div>;
+  const url = "/food_type_results?mealtype=appetizer";
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((exception) => console.error(exception));
+  }, []);
+  return (
+    <AppetizerContainer>
+      {data?.results?.length > 0 &&
+        data.results.map((result) => (
+          <AppetizerItemContainer>
+            <img src={result.image} alt={result.title}></img>
+            <div>{result.title}</div>
+          </AppetizerItemContainer>
+        ))}
+    </AppetizerContainer>
+  );
 };
--- client/src/components/Dinner/SideDish.js
+++ client/src/components/Dinner/SideDish.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const SideDishContainer = styled.div`
+  height: 250vh;
+  width: 100vw;
+  background-color: #cfebfd;
+`;
+
+const SideDishItemContainer = styled.div`
+  display: inline-block;
+`;
 
 export const SideDish = () => {
-  return <div></div>;
+  const url = "/food_type_results?mealtype=side dish";
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((exception) => console.error(exception));
+  }, []);
+  return (
+    <SideDishContainer>
+      {data?.results?.length > 0 &&
+        data.results.map((result) => (
+          <SideDishItemContainer>
+            <img src={result.image} alt={result.title}></img>
+            <div>{result.title}</div>
+          </SideDishItemContainer>
+        ))}
+    </SideDishContainer>
+  );
 };
